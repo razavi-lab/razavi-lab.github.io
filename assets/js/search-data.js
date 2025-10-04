db767943639e46b7dcb97daa4f19acae4ad03b4a
razavi-lab <razavilab9@gmail.com>
2025-10-04 23:47:53 +0000

    Deploying to gh-pages from @ razavi-lab/razavi-lab.github.io@80d9986b5ab17719b7bc9d7433cad50391b6fa60 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
+  },{id: "nav-people",
+          title: "People",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/members/";
+          },
+        },{id: "nav-projects",
           title: "Projects",
           description: "",
           section: "Navigation",
